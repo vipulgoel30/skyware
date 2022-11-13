5e9a82d062b54b22cd530fa967c713cceee7cb81
Sahil <aggarwalsahil2004@gmail.com>
2022-11-13 21:22:19 +0530

    commit

--- src/Components/Career/Career.js
+++ src/Components/Career/Career.js
@@ -64,22 +64,22 @@ export default function Career() {
 
     return (
         <div className='flex flex-col items-center gap-y-16 mb-16'>
-            <div className='relative max-w-fit w-[90%]'>
-                <img src='/images/careers.jpeg' alt='' className='w-full aspect-video max-h-[87.5vh]' />
+            <div className='relative max-w-fit w-[95%]'>
+                <img src='/images/careers.jpeg' alt='' className='w-full aspect-[1.25] sm:aspect-video max-h-[87.5vh]' />
                 <div className='absolute inset-0 bg-white bg-opacity-40 z-10'>
-                    <div className='absolute left-10 top-1/2 -translate-y-1/2 space-y-8'>
-                        <h2 className='font-bold text-2xl sm:text-3xl md:text-4xl'>Get Jobs</h2>
+                    <div className='absolute left-10 right-10 top-1/2 -translate-y-1/2 space-y-8'>
+                        <h2 className='font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>Get Jobs</h2>
                         <div className='flex flex-wrap gap-x-4 gap-y-2'>
-                            <input ref={title} placeholder='Job title' className='bg-white bg-opacity-30 text-lg sm:text-xl border border-black rounded-lg px-2 py-1 placeholder:text-black active:outline-none focus:outline-none' />
-                            <select ref={location} placeholder='Location' className='bg-white bg-opacity-30 text-lg sm:text-xl border border-black rounded-lg px-2 py-1 active:outline-none focus:outline-none'>
+                            <input ref={title} placeholder='Job title' className='bg-white bg-opacity-30 sm:text-lg md:text-xl border border-black rounded-lg px-2 py-1 placeholder:text-black active:outline-none focus:outline-none' />
+                            <select ref={location} placeholder='Location' className='bg-white bg-opacity-30 sm:text-lg md:text-xl border border-black rounded-lg px-2 py-1 active:outline-none focus:outline-none'>
                                 <option value=''>Location</option>
                                 <option value='Bangalore, India' className='bg-red'>Bangalore, India</option>
                                 <option value='Mumbai, India'>Mumbai, India</option>
                                 <option value='New Delhi, India'>New Delhi, India</option>
                             </select>
                             <div className='space-x-4'>
-                                <button className='text-xl rounded-3xl cursor-pointer transition-all duration-500 px-5 py-1.5 font-mediu text-black bg-white bg-opacity-30 border border-black hover:scale-105 active:outline-none focus:outline-none' onClick={() => setFilter(title.current.value || location.current.value)}>Search</button>
-                                {filter && <button className='text-xl rounded-3xl cursor-pointer transition-all duration-500 px-5 py-1.5 font-mediu text-black bg-white bg-opacity-30 border border-black hover:scale-105 active:outline-none focus:outline-none' onClick={reset}>Reset</button>}
+                                <button className='sm:text-lg md:text-xl rounded-3xl cursor-pointer transition-all duration-500 px-5 py-1.5 font-mediu text-black bg-white bg-opacity-30 border border-black hover:scale-105 active:outline-none focus:outline-none' onClick={() => setFilter(title.current.value || location.current.value)}>Search</button>
+                                {filter && <button className='sm:text-lg md:text-xl rounded-3xl cursor-pointer transition-all duration-500 px-5 py-1.5 font-mediu text-black bg-white bg-opacity-30 border border-black hover:scale-105 active:outline-none focus:outline-none' onClick={reset}>Reset</button>}
                             </div>
                         </div>
                     </div>
@@ -87,11 +87,11 @@ export default function Career() {
             </div>
             <div className='space-y-8 px-8'>
                 <div className='flex flex-wrap gap-x-12 gap-y-6 justify-between items-center'>
-                    <h2 className='font-bold text-4xl'>{filter ? 'Filtered' : selectedTag} Jobs</h2>
+                    <h2 className='font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>{filter ? 'Filtered' : selectedTag} Jobs</h2>
                     <ul className='gap-x-3 gap-y-2 flex flex-wrap self-end'>
                         {allTags.map(tag => {
                             const selected = tag === selectedTag
-                            return <li key={tag} className={`relative text-xl rounded-3xl cursor-pointer transition-all duration-300 px-5 py-1.5 font-medium text-white border-2 border-green-400 bg-green-400 ${selected ? 'pl-14' : 'hover:text-green-400 hover:bg-white'}`} onClick={() => setSelectedTag(tag)}>
+                            return <li key={tag} className={`relative sm:text-lg md:text-xl rounded-3xl cursor-pointer transition-all duration-300 px-5 py-1.5 font-medium text-white border-2 border-green-400 bg-green-400 ${selected ? 'pl-14' : 'hover:text-green-400 hover:bg-white'}`} onClick={() => setSelectedTag(tag)}>
                                 {selected && <img className='absolute top-1/2 -translate-y-1/2 left-4 h-2/3 aspect-square' alt="" src='/images/selected.svg' />}
                                 {tag}
                             </li>
@@ -104,7 +104,7 @@ export default function Career() {
                             <RiSuitcaseFill className='scale-[2.25]' />
                         </div>
                         <div className=''>
-                            <div className='text-xl font-bold'>{title}</div>
+                            <div className='text-lg sm:text-xl font-bold'>{title}</div>
                             <div className='mt-2'>{desc}</div>
                             <div className='font-bold'>₹{stipend}/month</div>
                             <span className='pr-4'>{location}</span>
