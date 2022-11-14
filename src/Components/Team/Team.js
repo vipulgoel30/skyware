edb0a5135db3484101757976c9b9e9eb750c11f2
Sahil <aggarwalsahil2004@gmail.com>
2022-11-14 21:41:39 +0530

    commit

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -5,20 +5,20 @@ const icons = [<FaLinkedin />, <FaTwitter />];
 
 const team = {
     Leadership: [
-        { name: 'John Doe', title: 'CEO of Skyware', links: ['https://example.com/', 'https://example.com/'] },
-        { name: 'John Doe', title: 'Co-Founder of Skyware', links: ['https://example.com/', null] },
-        { name: 'John Doe', title: 'Supervisor', links: ['https://example.com/', 'https://example.com/'] },
+        { name: 'Alexander Hipp', image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29uYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=10', title: 'CEO of Skyware', links: ['https://example.com/', 'https://example.com/'] },
+        { name: 'Balor Everhart', image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnNvbmF8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=10', title: 'Co-Founder of Skyware', links: ['https://example.com/', null] },
+        { name: 'Isa Travert', image: 'https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbmF8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=10', title: 'Supervisor', links: ['https://example.com/', 'https://example.com/'] },
     ],
     Development: [
-        { name: 'John Doe', title: 'CTO of Skyware', links: ['https://example.com/', null] },
-        { name: 'John Doe', title: 'Development Manager', links: ['https://example.com/', 'https://example.com/'] }
+        { name: 'Ayanda Oriol', image: 'https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbmF8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=10', title: 'CTO of Skyware', links: ['https://example.com/', null] },
+        { name: 'Shiva Das', image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHBlcnNvbmF8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=10', title: 'Development Manager', links: ['https://example.com/', 'https://example.com/'] }
     ],
     Marketing: [
-        { name: 'John Doe', title: 'Marketing Manager', links: ['https://example.com/', 'https://example.com/'] }
+        { name: 'Mariana Hobbes', image: 'https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVyc29uYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=10', title: 'Marketing Manager', links: ['https://example.com/', 'https://example.com/'] }
     ],
     Services: [
-        { name: 'John Doe', title: 'Service Manager', links: ['https://example.com/', 'https://example.com/'] },
-        { name: 'John Doe', title: 'Consultant', links: ['https://example.com/', null] },
+        { name: 'Nela Zlatkov', image: 'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=10', title: 'Service Manager', links: ['https://example.com/', 'https://example.com/'] },
+        { name: 'Gotama Key', title: 'Consultant', links: ['https://example.com/', null] },
     ]
 }
 
@@ -33,9 +33,9 @@ export default function Team() {
                 {Object.keys(team).map((field, i) => <div key={i} className="flex flex-col gap-6">
                     <h2 className="w-max mx-auto uppercase text-2xl border-b px-1 font-bold">{field}</h2>
                     <div className="flex flex-wrap gap-8 w-11/12 mx-auto">
-                        {team[field].map(({ name, title, links }, i) => <div key={i} className="mx-auto grid place-content-center text-center">
+                        {team[field].map(({ name, image, title, links }, i) => <div key={i} className="mx-auto grid place-content-center text-center">
                             <div className="flex items-center justify-center relative bg-white w-44 aspect-square rounded-full overflow-hidden">
-                                <img loading="eager" src='/images/dummy.webp' alt="" />
+                                <img loading="eager" src={image || '/images/dummy.webp'} alt="" />
                                 {links && <div className="absolute inset-0 bg-black bg-opacity-25 opacity-0 hover:opacity-100 flex items-center justify-evenly gap-2 p-2 transition-all duration-300">
                                     {links.map((link, i) => link && <a key={i} href={link} target="_blank" rel="noreferrer">
                                         <span className={`grid place-content-center text-lg text-[#343434]/70 p-3 bg-white rounded-[50%] ${i ? 'hover:bg-blue-400' : 'hover:bg-blue-600'} hover:text-white transition-all duration-300`}>
